833812effc5d52b9ff8ea87202c176f727e54e62
TaniSam <87119371+TaniSam@users.noreply.github.com>
2023-04-21 14:20:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function draw(){
+
+}
+ 
+function setup(){
+    canvas= createCanvas(300,300)
+    canvas.center()
+    canvas.position(610, 250)
+}
+
+function snap(){
+    save ("snap.png")
+}
